d53b8f0bb97f9761124764ad6c574b510181c71a
William Wong <william.wai.lim.wong@gmail.com>
2023-04-02 21:37:23 -0400

    Completed array map method lab

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  let tutorialsCased = tutorials.map(capFirstLetter);
+  function capFirstLetter(str){
+    const arr = str.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    const str2 = arr.join(" ");
+    return str2;
+    
+  }
+ 
+
+  return tutorialsCased
+  
 }
